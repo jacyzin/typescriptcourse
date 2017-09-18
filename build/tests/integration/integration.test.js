205f3aa745d9520233517641569900f330ba26ff
Henry Jacyzin <henryjacyzin@gmail.com>
2017-09-18 00:51:04 +0000

    aula 57

--- build/tests/integration/integration.test.js
+++ build/tests/integration/integration.test.js
@@ -3,12 +3,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var helpers_1 = require("./config/helpers");
 var HTTPStatus = require("http-status");
 describe('Testes de Integração', function () {
+    'use strict';
+    var config = require('../../server/config/env/config');
+    var model = require('../../server/models');
+    var id;
+    var userTest = {
+        id: 100,
+        name: 'Henry',
+        email: 'jacyzin@live.com',
+        password: 'teste'
+    };
+    var userDefault = {
+        id: 1,
+        name: 'Walquiria',
+        email: 'walquiria@asgard.net',
+        password: 'Od1n'
+    };
+    beforeEach(function (done) {
+        model.User.destroy({
+            where: {}
+        })
+            .then(function () {
+            return model.User.create(userDefault);
+        });
+    });
     describe('GET /api/users/all', function () {
         it('Deve retornar um json com todos os Usuários', function (done) {
             helpers_1.request(helpers_1.app)
                 .get('/api/users/all')
                 .end(function (error, res) {
                 helpers_1.expect(res.status).to.equal(HTTPStatus.OK);
+                helpers_1.expect(res.body.payload).to.be.an('array');
+                helpers_1.expect(res.body.payload[0].name).to.be.equal(userDefault.name);
+                helpers_1.expect(res.body.payload[0].email).to.be.equal(userDefault.email);
                 done(error);
             });
         });
@@ -30,13 +57,19 @@ describe('Testes de Integração', function () {
     describe('POST /api/users/create', function () {
         it('Deve adicionar um novo Usuário', function (done) {
             var user = {
-                nome: 'Teste'
+                id: 2,
+                name: 'Teste',
+                email: 'usuario@email.com',
+                password: 'novouser'
             };
             helpers_1.request(helpers_1.app)
                 .post('/api/users/create')
                 .send(user)
                 .end(function (error, res) {
                 helpers_1.expect(res.status).to.equal(HTTPStatus.OK);
+                helpers_1.expect(res.body.payload.id).to.be.eql(user.id);
+                helpers_1.expect(res.body.payload.name).to.be.eql(user.name);
+                helpers_1.expect(res.body.payload.email).to.be.eql(user.email);
                 done(error);
             });
         });
--- tests/integration/integration.test.ts
+++ tests/integration/integration.test.ts
@@ -3,12 +3,45 @@ import * as HTTPStatus from 'http-status';
 
 describe('Testes de Integração', () => {
 
+    'use strict';
+    const config = require('../../server/config/env/config');
+    const model = require('../../server/models');
+
+    let id;
+
+    const userTest = {
+        id:100,
+        name: 'Henry',
+        email: 'jacyzin@live.com',
+        password: 'teste'
+    };
+
+    const userDefault = {
+        id:1,
+        name: 'Walquiria',
+        email: 'walquiria@asgard.net',
+        password: 'Od1n'
+    };
+
+    beforeEach((done) => {
+        model.User.destroy({
+            where: {}
+        })
+        .then(() => {
+            return model.User.create(userDefault);
+        })
+    });
+
+
     describe('GET /api/users/all', () => {
         it('Deve retornar um json com todos os Usuários', done => {
             request(app)
             .get('/api/users/all')
             .end((error, res) => {
                 expect(res.status).to.equal(HTTPStatus.OK);
+                expect(res.body.payload).to.be.an('array');
+                expect(res.body.payload[0].name).to.be.equal(userDefault.name);
+                expect(res.body.payload[0].email).to.be.equal(userDefault.email);
                 done(error);
             })
         });
@@ -32,13 +65,19 @@ describe('Testes de Integração', () => {
     describe('POST /api/users/create', () => {
         it('Deve adicionar um novo Usuário', done => {
             const user = {
-                nome: 'Teste'
+                id: 2,
+                name: 'Teste',
+                email: 'usuario@email.com',
+                password: 'novouser'
             }
             request(app)
             .post('/api/users/create')
             .send(user)
             .end((error, res) => {
                 expect(res.status).to.equal(HTTPStatus.OK);
+                expect(res.body.payload.id).to.be.eql(user.id);
+                expect(res.body.payload.name).to.be.eql(user.name);
+                expect(res.body.payload.email).to.be.eql(user.email);
                 done(error);
             })
         });
